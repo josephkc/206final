b749063a1ddc3d8a768e988e04a59439dd42baa5
wucaro <wucaro@umich.edu>
2019-04-14 16:49:43 -0400

    added database file

--- health.py
+++ health.py
@@ -2,7 +2,7 @@ from yelp.client import Client
 import json
 import requests
 import sqlite3
-
+import re
 
 MY_API_KEY = "2DpBTLaOVW58XXsNb3Co7v16k4HbdCfYT1-Fv-OUSubypgNCyBRjQky5xCg97GfIYqfEDu4grwKNecEw5hSdC4AuPV7LOxr-0oCzXrWbm_19lKN_pefao5kEarGqXHYx" 
 headers = {'Authorization': 'Bearer %s' % MY_API_KEY}
@@ -14,11 +14,30 @@ client = Client(MY_API_KEY)
 req=requests.get(url, params=params, headers=headers)
  
 # proceed only if the status code is 200
-print('The status code is {}'.format(req.status_code))
- 
+#print('The status code is {}'.format(req.status_code))
+
 
 # printing the text from the response 
-json.loads(req.text)
-print (req.text)
+yelp_obj  =json.loads(req.text)
+yelpdata_dict = {'restaurant': yelp_obj['businesses'][0]['name'], 'cuisine': yelp_obj['businesses'][0]['categories'][0]['alias'], 'location': params['location']}
+
+#create connection with database
+
+conn = sqlite3.connect('yelp.sqlite')
+cur = conn.cursor()
+
+cur.execute("DROP TABLE IF EXISTS Yelp")
+cur.execute('CREATE TABLE Yelp (restaurant TEXT, cuisine TEXT, location TEXT)')
+
+for restaurant in yelpdata_dict:
+    restaurant = yelpdata_dict['restaurant']
+    cuisine = yelpdata_dict['cuisine']
+    location = yelpdata_dict['location']
+
+    cur.execute('''INSERT OR IGNORE INTO Yelp (restaurant, cuisine, location)
+        VALUES ( ?, ?, ? )''', ( restaurant, cuisine, location ) )
+conn.commit()
+
+#print (req.text)
 
 
