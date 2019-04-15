8ad92c7cdeec44dff026445e16437752125e3290
Joseph Choi <joschoi@umich.edu>
2019-04-15 13:19:30 -0400

    renamed to menus

---
+++ menus_vis.py
@@ -0,0 +1,6 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+py.tools.set_credentials_file(username = "josephkc", api_key="3Q6YJh0ZIkkQuKBdRKb6")
+
+trace0 = go.Scatter
--- restaurants_vis.py
+++
@@ -1,4 +0,0 @@
-import plotly.plotly as py
-import plotly.graph_objs as go
-
-py.tools.set_credentials_file(username = "", api_key="")
