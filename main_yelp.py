f970f670a87f8ecb765cbaeb85ba8b6343c4fd59
wucaro <wucaro@umich.edu>
2019-04-21 23:00:08 -0400

    json file, visualization updated

--- main_yelp.py
+++ main_yelp.py
@@ -1,15 +1,17 @@
 from health import *
 from calculations_yelp import *
 
+
 if __name__ == "__main__":
     soup = getSoupObjFromURL("https://www.michigan-demographics.com/cities_by_population")
     conn = sqlite3.connect(r"\Users\Owner'\Documents\si206\206final\nutrition.sqlite")
     cur = conn.cursor()
 
-    
-
-    top_n = create_tuple(calculate(conn, cur), 20)
-    visualize(top_n)
+    #get_cities(soup)
+    #create_table(soup)
+    calculate(conn, cur)
+    #top_n = create_tuple(calculate(conn, cur), 20)
+    #visualize(top_n)
 
 
     
